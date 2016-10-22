2b81186ca4ab209ba459a4546b815dc9bc778adf
Or Almog <almogo@mcmaster.ca>
2016-10-22 17:25:31 -0400

    coord noise

--- Coord.java
+++ Coord.java
@@ -5,6 +5,9 @@ public class Coord {
 	double x;
 	double y;
 
+	static final double mltplr = 188;
+	static final double stddev = 0.3;
+
 	public Coord(){
 		this.x = 0;
 		this.y = 0;
@@ -15,10 +18,16 @@ public class Coord {
 		this.y = y;
 	}
 
-	public double noise(Transceiver sender, Transceiver recv){
+	public double noise(Transceiver recv, Message m){
 
+		double dist = getDist(m.getLocation(), recv.getLocation());
 
+		double sd = (c / (dist * Math.sqrt(2 * Math.pow(stddev, 2) * Math.PI)))
+		sd *= Math.exp((-1)*( Math.pow(m.getFreq() - recv.getListenFreq(), 2) )/( 2 * Math.pow(stddev, 2) )));
 
 	}
 
+	private double getDist(Coord a, Coord b){
+		return Math.sqrt(Math.pow(a.x-b.x, 2) + Math.pow(a.y-b.y, 2));
+	}
 }
