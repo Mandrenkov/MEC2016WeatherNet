7a548c53a2374d6e354f1d877104129037f86e90
Ian Prins <prinsij@mcmaster.ca>
2016-10-22 17:22:57 -0400

    Add freq

--- Message.java
+++ Message.java
@@ -3,11 +3,14 @@ public class Message {
     private Coord position;
     private int senderID;
     private String content, type;
-    Message(int time, Coord postion, int senderID, String content, String type){
+    private double freq;
+    public static final String CHATTER = "chatter";
+    Message(int time, Coord postion, int senderID, String content, String type, double freq){
         this.time = time;
         this.position = position;
         this.senderID = senderID;
         this.content = content;
         this.type = type;
+        this.freq = freq;
     }
 }
