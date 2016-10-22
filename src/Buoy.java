1c326d0d23bc0779f7624a7eed0575241a9cca60
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2016-10-22 18:22:29 -0400

    Missing Curly Brace in Buoy

--- src/Buoy.java
+++ src/Buoy.java
@@ -8,11 +8,11 @@ public class Buoy extends Transceiver {
 
     public void receiveMessage(Message message) {
         System.out.println("buoy " + this.getID() + " received " + message.getContent());
-
+    }
+    
     public ArrayList<Message> sendMessages() {
         ArrayList<Message> list = new ArrayList<Message>();
-        list.add(new Message((int) System.currentTimeMillis(), this.location,
-                this.id, "hello world", Message.CHATTER, 1.0, 1.0));
+        list.add(new Message((int) System.currentTimeMillis(), this.location,this.id, "hello world", Message.CHATTER, 1.0, 1.0));
         return list;
     }
 }
