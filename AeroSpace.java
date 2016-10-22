7bedd558eb8ec72d25a3801421559aa4b63f53f6
Ian Prins <prinsij@mcmaster.ca>
2016-10-22 17:11:01 -0400

    Add AeroSpace

---
+++ AeroSpace.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class AeroSpace {
+    private double width, height;
+    private ArrayList<Transceiver> listeners;
+    private Queue<Message> messages;
+
+    public AeroSpace(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void addTransceiver(Transceiver t) {
+        listeners.add(t);
+    }
+
+    public void run() {
+        while (true) {
+            Message mess = messages.poll();
+            if (mess != null) {
+                for (Transceiver t : listeners) {
+                    t.processMessage(mess);
+                }
+            }
+            for (Transceiver t : listeners) {
+                messages.add(t.getMessage());
+            }
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args){
 
 		System.out.println("MEC 2016");
+		AeroSpace AS = new AeroSpace(500, 500);
 
 	}
 }
