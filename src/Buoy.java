ae4ed07aaf3a1bee751ab7c2076870adb0db5387
Ian Prins <prinsij@mcmaster.ca>
2016-10-22 17:54:17 -0400

    Add buoy class

---
+++ src/Buoy.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Ian on 2016-10-22.
+ */
+public class Buoy extends Transceiver {
+    private static final double listenFactor = 1.0, sendFactor = 1.0, listenFreq = 500.0, sendFreq = 200.0;
+
+    public Buoy(int id, Coord location) {
+        super(id, listenFactor, location, sendFactor, listenFreq, sendFreq);
+    }
+
+    public void receive(Message mess) {
+        System.out.println("buoy " + this.getID() + " received " + mess.getContent());
+    }
+
+    public Message send() {
+        return new Message((int) System.currentTimeMillis(), this.location,
+                this.id, "hello world", Message.CHATTER, 1.0, 1.0);
+    }
+}
