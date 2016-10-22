155a18694320b0df35c2dfe4eea2a76124a3e96a
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2016-10-22 18:24:46 -0400

    Added SendFrequency to Buoy

--- src/Boat.java
+++ src/Boat.java
@@ -3,9 +3,10 @@ import java.util.ArrayList;
 public class Boat extends Transceiver {
 
 	private String name;
+	private double sendFrequency;
 
-	public Boat(int id, double listenFactor, Coord location, String name, double sendFactor) {
-		super(id, listenFactor, location, sendFactor, 1);
+	public Boat(int id, double listenFactor, Coord location, String name, double sendFactor, double sendFrequency) {
+		super(id, listenFactor, location, sendFactor, sendFrequency);
 		this.name = name;
 	}
 
--- src/Buoy.java
+++ src/Buoy.java
@@ -2,14 +2,17 @@ import java.util.ArrayList;
 
 public class Buoy extends Transceiver {
 
+    private double sendFrequency;
+
     public Buoy(int id, double listenFactor, Coord location, double sendFactor, double sendFrequency) {
         super(id, listenFactor, location, sendFactor, sendFrequency);
+        this.sendFrequency = sendFrequency;
     }
 
     public void receiveMessage(Message message) {
         System.out.println("buoy " + this.getID() + " received " + message.getContent());
     }
-    
+
     public ArrayList<Message> sendMessages() {
         ArrayList<Message> list = new ArrayList<Message>();
         list.add(new Message((int) System.currentTimeMillis(), this.location,this.id, "hello world", Message.CHATTER, 1.0, 1.0));
